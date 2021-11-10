d8c5bee36bc60f40721c6f7a4c234e5ae6c1ba8c
dapoi <daffaprabowo5@gmail.com>
2021-11-10 18:27:26 +0700

    add reset password activity

---
+++ app/src/main/java/com/dafdev/selamatkan/view/activity/auth/ResetActivity.kt
@@ -0,0 +1,11 @@
+package com.dafdev.selamatkan.view.activity.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResetActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reset)
+    }
+}
