992cfcf7b6beff78fc803d8d8c291a4434b1a090
dapoi <daffaprabowo5@gmail.com>
2022-03-02 22:52:50 +0700

    update misc

--- app/src/main/java/com/dafdev/selamatkan/utils/HelpUtil.kt
+++ app/src/main/java/com/dafdev/selamatkan/utils/HelpUtil.kt
@@ -1,10 +1,16 @@
 package com.dafdev.selamatkan.utils
 
 import android.app.Activity
+import android.view.View
+import android.widget.ProgressBar
 import androidx.core.content.ContextCompat
 
 object StatusBarColor {
     fun setStatusBar(activity: Activity, color: Int) {
         activity.window.statusBarColor = ContextCompat.getColor(activity, color)
+    }
+
+    fun ProgressBar.showProgressBar(state: Boolean) {
+        visibility = if (state) View.VISIBLE else View.GONE
     }
 }
