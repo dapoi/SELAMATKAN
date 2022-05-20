6a322772f5215e54d24e4fba79705d144513dd38
dapoi <daffaprabowo5@gmail.com>
2022-05-20 18:58:11 +0700

    fix status bar color

--- app/src/main/java/com/dafdev/selamatkan/view/activity/core/AboutActivity.kt
+++ app/src/main/java/com/dafdev/selamatkan/view/activity/core/AboutActivity.kt
@@ -3,7 +3,9 @@ package com.dafdev.selamatkan.view.activity.core
 import android.os.Bundle
 import android.text.method.LinkMovementMethod
 import androidx.appcompat.app.AppCompatActivity
+import com.dafdev.selamatkan.R
 import com.dafdev.selamatkan.databinding.ActivityAboutBinding
+import com.dafdev.selamatkan.utils.HelpUtil
 
 class AboutActivity : AppCompatActivity() {
 
@@ -14,7 +16,10 @@ class AboutActivity : AppCompatActivity() {
         binding = ActivityAboutBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.toolbar.setNavigationOnClickListener { onBackPressed() }
-        binding.tvDesc.movementMethod = LinkMovementMethod.getInstance()
+        binding.apply {
+            HelpUtil.setStatusBarColor(this@AboutActivity, R.color.white, root)
+            toolbar.setNavigationOnClickListener { onBackPressed() }
+            tvDesc.movementMethod = LinkMovementMethod.getInstance()
+        }
     }
 }
